42f8622e23cd502558f5d7bccb9eac18d463f284
fuele <37090676+fuele@users.noreply.github.com>
2018-07-30 02:42:43 +0000

    Added flushing unit tests

--- netaggro/length_buffer.py
+++ netaggro/length_buffer.py
@@ -10,23 +10,42 @@ class LengthBuffer:
         self.max_size = max_size
     #end function
 
+    def register_reducer(self,r):
+        """
+        
+        """
+        self.reducers = r
 
     def add(self,obj):
         """
         Adds the object to the buffer's collection. Adding more than the 
         max_size will cause the buffer to flush
         """
-
         if self.max_size == None or self.max_size < 0:
             raise AttributeError('Must set max_size to an int greater than or equal to 0')
 
         self.buffer.append(obj)
+
+        if len(self.buffer) > self.max_size:
+            self.flush()
     #end function
+
+    def flush(self):
+        """
+        Sends everything in the buffer to the reducer. Clears the buffer.
+        """
+        self.reducers.process(self.buffer)
+
+        self.buffer=list()
+    #end function
+
 
 
     def __len__(self):
         return len(self.buffer)
     #end function
+
+
 
 
 #end class
--- test/test_length_buffer.py
+++ test/test_length_buffer.py
@@ -1,18 +1,21 @@
-import unittest
+from unittest import TestCase
 from netaggro import length_buffer
+from unittest.mock import patch, Mock, MagicMock
 
-class TestLengthBuffer(unittest.TestCase):
+class TestLengthBuffer(TestCase):
 
     #def test_datetime(self):
     #    bucket.datetime(None, None)
     #    self.assertTrue(True)
 
 
-    def test_add(self):
+    @patch('netaggro.reducer.Reducer')
+    def test_add(self,mock_reducer):
         buff = length_buffer.LengthBuffer(5)
         self.assertEqual(len(buff),0)
         buff.add({'id':'test1'})
         self.assertEqual(len(buff),1)
+        mock_reducer.process.assert_not_called()
     #end function
 
     def test_adding_with_no_max_size_throws_exception(self):
@@ -36,10 +39,27 @@ class TestLengthBuffer(unittest.TestCase):
         self.assertTrue(context)
     #end function
 
+    @patch('netaggro.reducer.Reducer')
+    def test_adding_item_forces_flush(self,mock_reducer):
+        #mock_reducer = Mock()
+        #mock_reducer.process = MagicMock()
+
+        obj = {'id':'test'}
+
+        buff = length_buffer.LengthBuffer(0)
+        buff.register_reducer(mock_reducer)
+        buff.add(obj)
+        
+        self.assertEqual(len(buff),0)
+        mock_reducer.process.assert_called_once_with([obj])
+    #end function
+
+
 
     
 
 #end class
+
 
 if __name__=='__main__':
     unittest.main()
